799eb9526b26b8729a31772c68b4f3fbeeee4d32
AxelNAY <axelnay@sfr.fr>
2024-04-19 09:54:38 +0000

    Update the file shell_inter.c

--- shell_inter.c
+++ shell_inter.c
@@ -11,15 +11,34 @@
 int main(void)
 {
 	char *line = NULL;
+	char *prt = "$ ";
 	size_t len = 0;
 	ssize_t read;
 
-	printf("$ ");
-	while ((read = getline(&line, &len, stdin)) != -1)
+
+	while (1)
 	{
-		printf("%s", line);
+		if (isatty(fileno(stdin))
+				printf("%s", prt);
+
+		read = getline(&line, &len, stdin);
+		if (read == -1)
+		{
+			free(line);
+			exit(EXIT_FAILURE);
+		}
+
+		if (line == NULL)
+			continue;
+
+		if (len > 0 && line[read - 1] == '\n')
+			line[read - 1] = '\0';
+
+		/*call process command in the file tokenize.c*/
+		
+
 	}
-	free(line);
+
 
 	return (0);
 }
