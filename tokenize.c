a30fa6a56565bed42b4155a29ed368a1c5c12f1d
Raph6n <naim.raphael@outlook.fr>
2024-04-22 09:28:17 +0000

    tokenize and task 3

---
+++ tokenize.c
@@ -0,0 +1,44 @@
+#include "shell.h"
+
+void command (char *line)
+{
+char *token[64];
+int count = 0;
+const char *specials = " \t\n";
+pid_t pid;
+int status;
+
+ 
+if (line == NULL)
+return;
+
+char *token = strtok(line, specials);
+
+while (token != NULL && i < 64)
+{
+token[i++] = token;
+token = strtok(NULL, specials);
+}
+token[i] = NULL;
+
+if (token[0] = NULL)
+return;
+
+pid = fork ();
+
+if (pid == -1)
+{
+perror ("fork");
+return;
+}
+else if (pid == 0)
+{
+execvp(token[0], token);
+fprintf(stderr, "erreur %s\n", token[0]);
+exit(EXIT_FAILURE);
+}
+else
+{
+waitpid(pid, &status, 0);
+}
+}
