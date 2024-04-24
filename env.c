c9b8caeac4fefb442af810bfbecff16d1fe5a694
AxelNAY <axelnay@sfr.fr>
2024-04-24 13:06:57 +0000

    betty checked for all the files

--- env.c
+++ env.c
@@ -2,12 +2,13 @@
 
 void print_env(void)
 {
-int count = 0;
- extern char **environ;
-while (environ[count] != NULL)
-{
-printf("%s\n", environ[count]);
-count++;
-}
+	int count = 0;
+	extern char **environ;
+
+	while (environ[count] != NULL)
+	{
+		printf("%s\n", environ[count]);
+		count++;
+	}
 }
   
